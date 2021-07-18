14626a222f64cf4aaee651ce34a8cdceb5276a0a
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-07-18 12:04:34 -0700

    Configure TailwindCSS

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import NextDocument, { Html, Head, Main, NextScript } from "next/document"
+
+export default class Document extends NextDocument {
+  render(): JSX.Element {
+    return (
+      <Html lang="en" className="nightwind">
+        <Head>
+        </Head>
+        <body className="max-w-7xl mx-auto bg-gray-50">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,42 @@
+const plugin = require("tailwindcss/plugin")
+
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  mode: "jit",
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./utils/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+    require("nightwind"),
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        ".h-navbar": {
+          height: "69px",
+        },
+        ".top-navbar": {
+          top: "69px",
+        },
+        ".navbar-link": {
+          "@apply text-sm font-medium text-black opacity-70 hover:opacity-100": {},
+        },
+        ".bg-blur": {
+          "@apply bg-opacity-50 bg-gray-50 dark:bg-opacity-50 backdrop-filter backdrop-blur": {},
+        },
+        ".px-body": {
+          "@apply px-4 sm:px-6 xl:px-8": {},
+        },
+      })
+    }),
+  ],
 }
