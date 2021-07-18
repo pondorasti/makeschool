66add8fad03cfd3526dd99597b841418f10d92fc
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-07-18 11:02:07 -0700

    Configure Courses for hosting

--- next.config.js
+++ next.config.js
@@ -1,3 +1,38 @@
 module.exports = {
-  reactStrictMode: true,
+  rewrites: async () => {
+    return [
+      {
+        source: "/courses/BEW-1.1",
+        destination: "/courses/BEW-1.1/index.html",
+      },
+      {
+        source: "/courses/BEW-1.2",
+        destination: "/courses/BEW-1.2/index.html",
+      },
+      {
+        source: "/courses/BEW-1.3",
+        destination: "/courses/BEW-1.3/index.html",
+      },
+      {
+        source: "/courses/BEW-2.1",
+        destination: "/courses/BEW-2.1/index.html",
+      },
+      {
+        source: "/courses/BEW-2.2",
+        destination: "/courses/BEW-2.2/index.html",
+      },
+      {
+        source: "/courses/BEW-2.3",
+        destination: "/courses/BEW-2.3/index.html",
+      },
+      {
+        source: "/courses/BEW-2.4",
+        destination: "/courses/BEW-2.4/index.html",
+      },
+      {
+        source: "/courses/BEW-2.5",
+        destination: "/courses/BEW-2.5/index.html",
+      },
+    ]
+  },
 }
