19534f858953aa6f80e12d48f7aaf35d205f6d1b
CHRIS_LEE <chrismlee87@gmail.com>
2021-07-20 01:34:00 -0700

    Courses Added for MOB, DS, CS, SPD &  WEB

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,14 +18,15 @@ const concentrations: IConcentration[] = [
     name: "Front End Web Concentration",
     courses: [
       {
+        // Technically there was no FEW1.1 it was WEB1.1. Also there was a WEB1.0.
         name: "FEW 1.1",
-        description: "Foundation Web",
-        instructor: "Mitchell",
+        description: "Web Foundations",
+        instructor: "Meredith",
         link: "/courses/FEW-1.1",
       },
       {
         name: "FEW 1.2",
-        description: "Foundation JavaScript",
+        description: "JavaScript Foundations",
         instructor: "Mitchell",
         link: "/courses/FEW-1.2",
       },
@@ -43,31 +44,31 @@ const concentrations: IConcentration[] = [
       },
       {
         name: "FEW 2.2",
-        description: "Advanced CSS and Visual Design",
+        description: "Advanced CSS and Web Design",
         instructor: "Mitchell",
         link: "/courses/FEW-2.2",
       },
       {
         name: "FEW 2.3",
-        description: "Single Page Applications",
+        description: "Single Page Web Applications",
         instructor: "Mitchell",
         link: "/courses/FEW-2.3",
       },
       {
         name: "FEW 2.4",
-        description: "React Native",
+        description: "Native Development with JavaScript",
         instructor: "Mitchell",
         link: "/courses/FEW-2.4",
       },
       {
         name: "FEW 2.5",
-        description: "Data & Visualization",
+        description: "Data Visualization & Web Graphics",
         instructor: "Mitchell",
         link: "/courses/FEW-2.5",
       },
       {
         name: "FEW 2.9",
-        description: "GraphQL",
+        description: "Advanced Technical Seminar (GraphQL)",
         instructor: "Mitchell",
         link: "/courses/FEW-2.9",
       },
@@ -76,10 +77,11 @@ const concentrations: IConcentration[] = [
   {
     name: "Back End Web Concentration",
     courses: [
+      // Technically there was no BEW1.1 it was WEB1.1
       {
         name: "BEW 1.1",
         description: "RESTful & Resourceful MVC Architecture",
-        instructor: "Dani",
+        instructor: "Meredith",
         link: "/courses/BEW-1.1",
       },
       {
@@ -102,13 +104,13 @@ const concentrations: IConcentration[] = [
       },
       {
         name: "BEW 2.2",
-        description: "Docker, DevOps, & Deployments",
+        description: "DevOps, Deployment & Containers",
         instructor: "Dani",
         link: "/courses/BEW-2.2",
       },
       {
         name: "BEW 2.3",
-        description: "Intro to Web Security",
+        description: "Introduction to Web Security",
         instructor: "Dani",
         link: "/courses/BEW-2.3",
       },
@@ -120,9 +122,238 @@ const concentrations: IConcentration[] = [
       },
       {
         name: "BEW 2.5",
-        description: "Go - Patterns & Practices in Strongly Typed Languages",
+        description: "Go - Patterns & Practices in Strongly Typed Ecosystems",
         instructor: "Dani",
         link: "/courses/BEW-2.5",
+      },
+      {
+        name: "BEW 2.9",
+        description: "Advanced Technical Seminar (BEW)",
+        instructor: "Dani",
+        link: "/courses/BEW-2.9",
+      },
+    ],
+  },
+  {
+    name: "Mobile Development",
+    courses: [
+      {
+        name: "MOB 1.1",
+        description: "Introduction to Swift",
+        instructor: "Adriana",
+        link: "/courses/MOB-1.1",
+      },
+      {
+        name: "MOB 1.2",
+        description: "Introduction to iOS Development in Swift",
+        instructor: "Adriana",
+        link: "/courses/MOB-1.2",
+      },
+      {
+        name: "MOB 1.3",
+        description: "Dynamic iOS Applications",
+        instructor: "Adriana",
+        link: "/courses/MOB-1.3",
+      },
+      {
+        name: "MOB 2.1",
+        description: "Local Persistence in iOS",
+        instructor: "Adriana",
+        link: "/courses/MOB-2.1",
+      },
+      {
+        name: "MOB 2.2",
+        description: "2D Mobile Game Development",
+        instructor: "Adriana",
+        link: "/courses/MOB-2.2",
+      },
+      {
+        name: "MOB 2.3",
+        description: "Concurrency & Parallelism in iOS",
+        instructor: "Adriana",
+        link: "/courses/MOB-2.3",
+      },
+      {
+        name: "MOB 2.4",
+        description: "Advanced Architectural Patterns in iOS",
+        instructor: "Adriana",
+        link: "/courses/MOB-2.4",
+      },
+      {
+        name: "MOB 2.9",
+        description: "Advanced Technical Seminar (MOB)",
+        instructor: "Adriana",
+        link: "/courses/MOB-2.9",
+      },
+    ],
+  },
+  {
+    name: "Data Science",
+    courses: [
+      {
+        name: "DS 1.0",
+        description: "Data Exploration and Visualization",
+        instructor: "Joseph",
+        link: "/courses/DS-1.0",
+      },
+      {
+        name: "DS 1.11",
+        description: "Introduction to Data Science Workflow",
+        instructor: "Joseph",
+        link: "/courses/DS-1.11",
+      },
+      {
+        name: "DS 2.1",
+        description: "Machine Learning",
+        instructor: "Joseph",
+        link: "/courses/DS-2.1",
+      },
+      {
+        name: "DS 2.2",
+        description: "Deep Learning",
+        instructor: "Joseph",
+        link: "/courses/DS-2.2",
+      },
+      {
+        name: "DS 2.3",
+        description: "Data Science in Production",
+        instructor: "Joseph",
+        link: "/courses/DS-2.3",
+      },
+      {
+        name: "DS 2.4",
+        description: "Core Applications of Artificial Intelligence",
+        instructor: "Joseph",
+        link: "/courses/DS-2.4",
+      },
+      {
+        name: "DS 2.5",
+        description: "Natural Language Processing",
+        instructor: "Joseph",
+        link: "/courses/DS-2.5",
+      },
+      {
+        name: "DS 2.9",
+        description: "Advanced Technical Seminar (DS)",
+        instructor: "Joseph",
+        link: "/courses/DS-2.9",
+      },
+    ],
+  },
+  {
+    name: "Core Computer Science",
+    courses: [
+      {
+        name: "CS 1.0",
+        description: "Intro to Programming",
+        instructor: "Joi",
+        link: "/courses/CS-1.0",
+      },
+      {
+        name: "CS 1.1",
+        description: "Programming Fundamentals",
+        instructor: "Joi",
+        link: "/courses/CS-1.1",
+      },
+      {
+        name: "CS 1.2",
+        description: "How Data Structures Work",
+        instructor: "Joi",
+        link: "/courses/CS-1.2",
+      },
+      {
+        name: "CS 1.3",
+        description: "Core Data Structures and Algorithms",
+        instructor: "Joi",
+        link: "/courses/CS-1.3",
+      },
+      {
+        name: "CS 2.1",
+        description: "Advanced Trees & Sorting Algorithms",
+        instructor: "Joi",
+        link: "/courses/CS-2.1",
+      },
+      {
+        name: "CS 2.2",
+        description: "Advanced Recursion & Graphs",
+        instructor: "Joi",
+        link: "/courses/CS-2.2",
+      },
+    ],
+  },
+  {
+    name: "Software Product Development",
+    courses: [
+      {
+        name: "SPD 1.1",
+        description: "Introduction to Software Development",
+        instructor: "Braus",
+        link: "/courses/SPD-1.1",
+      },
+      {
+        name: "SPD 1.2",
+        description: "Agile Product Development & UI/UX",
+        instructor: "Braus",
+        link: "/courses/SPD-1.2",
+      },
+      {
+        name: "SPD 1.3",
+        description: "Team Software Project",
+        instructor: "Braus",
+        link: "/courses/SPD-1.3",
+      },
+      {
+        name: "SPD 1.41",
+        description: "Engineering Careers 1: Communication & Interviewing",
+        instructor: "Braus",
+        link: "/courses/SPD-1.41",
+      },
+      {
+        name: "SPD 1.5",
+        description: "Engineering Careers 2: Management and Leadership",
+        instructor: "Braus",
+        link: "/courses/SPD-1.5",
+      },
+      {
+        name: "SPD 2.1",
+        description: "Industry Collaboration Project Part 1",
+        instructor: "Braus",
+        link: "/courses/SPD-2.1",
+      },
+      {
+        name: "SPD 2.2",
+        description: "Industry Collaboration Project Part 2",
+        instructor: "Braus",
+        link: "/courses/SPD-2.2",
+      },
+      {
+        name: "SPD 2.3",
+        description: "Testing and Architecture",
+        instructor: "Braus",
+        link: "/courses/SPD-2.3",
+      },
+      {
+        name: "SPD 2.4",
+        description: "Onboarding into Companies",
+        instructor: "Braus",
+        link: "/courses/SPD-2.4",
+      },
+    ],
+  },
+  {
+    name: "Web Foundations",
+    courses: [
+      {
+        name: "WEB 1.0",
+        description: "Web Foundations",
+        instructor: "Meredith",
+        link: "/courses/WEB-1.0",
+      },
+      {
+        name: "WEB 1.1",
+        description: "RESTful & Resourceful MVC Architecture",
+        instructor: "Meredith",
+        link: "/courses/WEB-1.1",
       },
     ],
   },
