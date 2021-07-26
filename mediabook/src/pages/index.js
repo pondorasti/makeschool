5b416a970e116152d50e32b705851072de3c0b11
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-07-25 19:34:13 -0700

    Fiddle with baseUrl

--- mediabook/src/pages/index.js
+++ mediabook/src/pages/index.js
@@ -1,11 +1,6 @@
 import React from "react"
-import Layout from "@theme/Layout"
-import Gallery from "../components/gallery"
+import { Redirect } from "@docusaurus/router"
 
 export default function Home() {
-  return (
-    <Layout description="A collection of tech related tutorials.">
-      <Gallery />
-    </Layout>
-  )
+  return <Redirect to="/tutorials/" />
 }
