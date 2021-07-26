786bbd79a2a23d59acdcf8d962a7099861ace09a
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-07-25 21:00:18 -0700

    Fix sidebar inconsistencies

--- mediabook/sidebars.js
+++ mediabook/sidebars.js
@@ -1,22 +1,7 @@
 const nodeRedditClone = require("./tutorials/Node-Reddit-Clone/sidebar")
+const environmentSetup = require("./tutorials/Environment-Setup/sidebar")
 
 module.exports = {
   nodeRedditClone: nodeRedditClone,
-  environmentSetup: [
-    {
-      type: "category",
-      label: "Environment Setup",
-      collapsed: false,
-      items: [
-        "Environment-Setup/P00-Welcome",
-        "Environment-Setup/P01-Computer-And-Environment-Setup",
-        "Environment-Setup/P02-Terminal-Setup",
-        "Environment-Setup/P03-VS-Code-Instructions",
-        "Environment-Setup/P04-Git-Github",
-        "Environment-Setup/P05-iOS-Install-Instructions",
-        "Environment-Setup/P06-Academic-Policies",
-        "Environment-Setup/P07-Culture-of-Feedback",
-      ],
-    },
-  ],
+  environmentSetup: environmentSetup,
 }
