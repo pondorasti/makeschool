7b98aa0b82ee67212daffe090bd36bdf29d7bfdf
Dylan Player <dylanplayer@Dylans-MacBook-Pro.local>
2022-04-18 17:07:17 -0700

    Added Gif Search Tutorial

--- mediabook/sidebars.js
+++ mediabook/sidebars.js
@@ -1,7 +1,9 @@
-const nodeRedditClone = require("./tutorials/Node-Reddit-Clone/sidebar")
-const environmentSetup = require("./tutorials/Environment-Setup/sidebar")
+const nodeRedditClone = require("./tutorials/Node-Reddit-Clone/sidebar");
+const environmentSetup = require("./tutorials/Environment-Setup/sidebar");
+const gifSearch = require("./tutorials/Gif-Search/sidebar");
 
 module.exports = {
   nodeRedditClone: nodeRedditClone,
   environmentSetup: environmentSetup,
-}
+  gifSearch: gifSearch,
+};
